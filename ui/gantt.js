1ed717118fdd5608e5c5d3ab66b4bf2b7f1bf624
ibrar talab <ibrartalabkhan@gmail.com>
2026-08-28 08:42:19 +0500

    feat(modal): design the form for add new task

---
+++ ui/gantt.js
@@ -0,0 +1,8 @@
+// Select html elements
+const addNewTaskButton = document.getElementById("add-new-task-button");
+
+addNewTaskButton.addEventListener("click", () => {
+    const taskList = document.getElementsByClassName("todo-todos");
+
+    
+});
